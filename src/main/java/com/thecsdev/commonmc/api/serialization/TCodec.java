1b6931b7637350bf2ee07301f4e446d46208786b
TheCSDev <thecsdev@gmail.com>
2026-04-11 14:01:37 +0200

    Add LenientMapCodec

--- src/main/java/com/thecsdev/commonmc/api/serialization/TCodec.java
+++ src/main/java/com/thecsdev/commonmc/api/serialization/TCodec.java
@@ -5,14 +5,15 @@ import com.mojang.serialization.DataResult;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Utility {@link Codec} implementations and functions.
  */
-public final class TCodecs
+public final class TCodec
 {
 	// ==================================================
-	private TCodecs() {}
+	private TCodec() {}
 	// ==================================================
 	/**
 	 * {@link Codec} for {@link java.net.URI}s.
@@ -34,9 +35,26 @@ public final class TCodecs
 	 * encode/decode instead of returning an error for the entire {@link List}.
 	 * @param codec The {@link Codec} for the list's element type.
 	 * @param <A> The list's element type.
+	 * @throws NullPointerException If the argument is {@code null}.
 	 */
-	public static <A> @NotNull Codec<List<A>> lenientListOf(@NotNull Codec<A> codec) {
+	public static <A> @NotNull Codec<List<A>> lenientListOf(
+			@NotNull Codec<A> codec) throws NullPointerException {
 		return new LenientListCodec<>(codec).orElse(List.of());
+	}
+
+	/**
+	 * Returns a {@link Codec} for a {@link Map}, that is lenient when encoding/decoding,
+	 * and simply ignores any key-value pairs that fail to encode/decode instead
+	 * of returning an error for the entire {@link Map}.
+	 * @param keyCodec The {@link Codec} for the map's key type.
+	 * @param valueCodec The {@link Codec} for the map's value type.
+	 * @param <K> The {@link Map}'s key type.
+	 * @param <V> The {@link Map}'s value type.
+	 * @throws NullPointerException If an argument is {@code null}.
+	 */
+	public static <K, V> @NotNull Codec<java.util.Map<K, V>> lenientMap(
+			@NotNull Codec<K> keyCodec, @NotNull Codec<V> valueCodec) throws NullPointerException {
+		return new LenientMapCodec<>(keyCodec, valueCodec).orElse(java.util.Map.of());
 	}
 	// ==================================================
 }
