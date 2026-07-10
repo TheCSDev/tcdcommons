74b5763c5f48703dfeab44fb427d9b11a336a590
TheCSDev <thecsdev@gmail.com>
2026-07-11 00:39:24 +0200

    Update TTooltipSubjectStats.java
    Support for 'Item descriptions' tag system.

--- src/main/java/com/thecsdev/commonmc/api/client/gui/tooltip/TTooltipSubjectStats.java
+++ src/main/java/com/thecsdev/commonmc/api/client/gui/tooltip/TTooltipSubjectStats.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -14,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.Optional;
 
 import static com.thecsdev.commonmc.api.stats.IStatsProvider.getStatTypeName;
 import static net.minecraft.ChatFormatting.*;
@@ -81,19 +83,60 @@ final @ApiStatus.Internal class TTooltipSubjectStats extends TTooltipLabel
 	private static final @Nullable MutableComponent computeItemDescription(
 			@NotNull SubjectStats<?> stats) throws NullPointerException
 	{
-		if(stats.getSubject() instanceof Item || stats.getSubject() instanceof Block) {
-			final var sid  = stats.getSubjectID();
-			final var key  = String.format("lore.%s.%s", sid.getNamespace(), sid.getPath());
-			final var lang = Language.getInstance();
-			return lang.has(key) ? translatable(key) : null;
-		} else if(stats.getSubject() instanceof EntityType<?>) {
-			final var sid  = stats.getSubjectID();
-			final var key  = String.format("entity.%s.%s.description", sid.getNamespace(), sid.getPath());
-			final var lang = Language.getInstance();
-			return lang.has(key) ? translatable(key) : null;
-		} else {
-			return null;
+		//general preparation for all cases
+		final var lang = Language.getInstance();
+
+		//handle items
+		if(stats.getSubject() instanceof Item item)
+			return (MutableComponent) Optional.empty()
+					.or(() -> {
+						final var sid  = stats.getSubjectID();
+						final var key  = String.format("lore.%s.%s", sid.getNamespace(), sid.getPath());
+						return Optional.ofNullable(lang.has(key) ? translatable(key) : null);
+					})
+					.or(() -> item.getDefaultInstance().tags()
+							.map(TagKey::location)
+							.map(id -> String.format("tag.%s.%s.description", id.getNamespace(), id.getPath()))
+							.map(key -> lang.has(key) ? translatable(key) : null)
+							.filter(Objects::nonNull)
+							.findFirst()
+					)
+					.orElse(null);
+		//handle blocks
+		else if(stats.getSubject() instanceof Block block)
+			return (MutableComponent) Optional.empty()
+					.or(() -> {
+						final var sid  = stats.getSubjectID();
+						final var key  = String.format("lore.%s.%s", sid.getNamespace(), sid.getPath());
+						return Optional.ofNullable(lang.has(key) ? translatable(key) : null);
+					})
+					.or(() -> block.defaultBlockState().tags()
+							.map(TagKey::location)
+							.map(id -> String.format("tag.%s.%s.description", id.getNamespace(), id.getPath()))
+							.map(key -> lang.has(key) ? translatable(key) : null)
+							.filter(Objects::nonNull)
+							.findFirst()
+					)
+					.orElse(null);
+		//handle entities
+		else if(stats.getSubject() instanceof EntityType<?> entityType) {
+			return (MutableComponent) Optional.empty()
+					.or(() -> {
+						final var sid  = stats.getSubjectID();
+						final var key  = String.format("entity.%s.%s.description", sid.getNamespace(), sid.getPath());
+						return Optional.ofNullable(lang.has(key) ? translatable(key) : null);
+					})
+					.or(() -> entityType.builtInRegistryHolder().tags()
+							.map(TagKey::location)
+							.map(id -> String.format("tag.%s.%s.description", id.getNamespace(), id.getPath()))
+							.map(key -> lang.has(key) ? translatable(key) : null)
+							.filter(Objects::nonNull)
+							.findFirst()
+					)
+					.orElse(null);
 		}
+		//all other types return nothing
+		else return null;
 	}
 	// ==================================================
 }
