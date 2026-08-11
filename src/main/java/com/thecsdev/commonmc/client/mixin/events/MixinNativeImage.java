6c5b6d56f48c0d3a2d58e08d35c74036ef1e3b32
TheCSDev <thecsdev@gmail.com>
2026-08-11 13:01:27 +0200

    v5.5.6 for 26.1

--- src/main/java/com/thecsdev/commonmc/client/mixin/events/MixinNativeImage.java
+++ src/main/java/com/thecsdev/commonmc/client/mixin/events/MixinNativeImage.java
@@ -2,10 +2,6 @@ package com.thecsdev.commonmc.client.mixin.events;
 
 import com.mojang.blaze3d.platform.NativeImage;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.nio.ByteBuffer;
 
 /**
  * This {@link Mixin}'s sole purpose is to put image byte data loading up to
@@ -13,9 +9,13 @@ import java.nio.ByteBuffer;
  * the game even fails to validate PNG properly most the time anyways, so this
  * is kind of more of a bug fix in a way.
  */
-@Mixin(value = NativeImage.class)
+@Mixin(value = NativeImage.class, priority = -1000)
 public abstract class MixinNativeImage
 {
+	//come to think of it, this mixin is not needed anymore unlike before 5.X.
+	//should external image loading ever be needed in the future, this mixin
+	//will return. for now, it is best it not interfere with other mods.
+	/*
 	@Redirect(
 		method = "read(Lcom/mojang/blaze3d/platform/NativeImage$Format;Ljava/nio/ByteBuffer;)Lcom/mojang/blaze3d/platform/NativeImage;",
 		at = @At(
@@ -25,4 +25,5 @@ public abstract class MixinNativeImage
 		require = 0
 	)
 	private static void skipPngValidation(ByteBuffer buffer) {}
+	*/
 }
